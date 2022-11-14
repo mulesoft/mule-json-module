36b16aa359850be1fb963581e4ba7a95077bdfb0
Ignacio Giss <rgiss@mulesoft.com>
2022-11-14 17:38:48 -0300

    W-11998991: Fix JSON validation when there are trailing tokens after … (#127)

---
+++ src/test/java/org/mule/extension/BadObjectValidationDefaultTestCase.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.extension;
+
+import static org.junit.Assert.assertEquals;
+
+import org.mule.runtime.core.api.event.CoreEvent;
+
+import org.junit.Test;
+
+public class BadObjectValidationDefaultTestCase extends AbstractSchemaValidationTestCase {
+
+  private String json;
+
+  @Override
+  protected String getConfigFile() {
+    return "object-array-validation-config.xml";
+  }
+
+  @Override
+  protected void doSetUp() throws Exception {
+    json = doGetResource("inputs/bad-object.json");
+  }
+
+  @Test
+  public void validateDefaultBehaviour() throws Exception {
+
+    CoreEvent flowResult = flowRunner("validate").withPayload(json).run();
+    assertEquals(json, flowResult.getMessage().getPayload().getValue());
+  }
+}
