47a3a7ab9a1e3cba7a3d04fc302ec84c67e726ee
juan.flores <juan.flores@mulesoft.com>
2022-12-14 14:56:12 -0300

    Fix typo at javadoc

--- src/main/java/org/mule/module/json/internal/JsonSchemaValidator.java
+++ src/main/java/org/mule/module/json/internal/JsonSchemaValidator.java
@@ -193,8 +193,8 @@ public class JsonSchemaValidator {
      * Only one is allowed at a time, you cannot use both at the same time.
      *
      * @return a {@link JsonSchemaValidator}
-     * @throws ModuleException if {@link #schemaContent} and {@link #schemaLocation} both are empty or null then produce SCHEMA_NOT_FOUND error.
-     * @throws ModuleException if {@link #schemaContent} and {@link #schemaLocation} both have a content it produces SCHEMA_INPUT_ERROR error.
+     * @throws ModuleException if {@link #schemaContent} and {@link #schemaLocation} both are empty or nul, so throw SCHEMA_NOT_FOUND error.
+     * @throws ModuleException if {@link #schemaContent} and {@link #schemaLocation} both have a content, so throw   SCHEMA_INPUT_ERROR error.
      */
     public JsonSchemaValidator build() {
 
