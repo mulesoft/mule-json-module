70fe6b02310ebd3fe0939422232c5d4a339e1d39
GuidoGianfrate <ggianfrate@mulesoft.com>
2023-01-12 10:45:19 -0300

    W-12049491: Added License header to TestCommonVariablesUtil

--- src/test/java/org/mule/extension/TestCommonVariablesUtil.java
+++ src/test/java/org/mule/extension/TestCommonVariablesUtil.java
@@ -1,10 +1,16 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
 package org.mule.extension;
 
 public class TestCommonVariablesUtil {
 
-    public static final String SCHEMA_FSTAB_GOOD_JSON = "inputs/fstab-good.json";
-    public static final String SCHEMA_FSTAB_BAD_JSON = "inputs/fstab-bad.json";
-    public static final String SCHEMA_FSTAB_BAD2_JSON = "inputs/fstab-bad2.json";
-    public static final String SCHEMA_FSTAB_GOOD_INLINE_JSON = "inputs/fstab-good-inline.json";
-    public static final String SCHEMA_FSTAB_DUPLICATE_KEYS = "inputs/fstab-duplicate-keys.json";
+  public static final String SCHEMA_FSTAB_GOOD_JSON = "inputs/fstab-good.json";
+  public static final String SCHEMA_FSTAB_BAD_JSON = "inputs/fstab-bad.json";
+  public static final String SCHEMA_FSTAB_BAD2_JSON = "inputs/fstab-bad2.json";
+  public static final String SCHEMA_FSTAB_GOOD_INLINE_JSON = "inputs/fstab-good-inline.json";
+  public static final String SCHEMA_FSTAB_DUPLICATE_KEYS = "inputs/fstab-duplicate-keys.json";
 }
